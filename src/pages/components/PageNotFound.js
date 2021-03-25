a5a9e56124f1cbb2a49f9e49490b10172a7f09b5
elvishuges <elvishuges@hotmail.com>
2021-03-25 19:28:33 -0300

    add github actions e pageNotFound 404

--- src/pages/components/PageNotFound.js
+++ src/pages/components/PageNotFound.js
@@ -1,39 +1,59 @@
-import { Link } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Link } from "react-router-dom";
 import React from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { useHistory } from "react-router";
 
-import { logoutUser } from './../../actions/auth.action'
+import { Helmet } from "react-helmet";
+import { Box, Container, Typography } from "@material-ui/core";
 
+import { logoutUser } from "./../../actions/auth.action";
 
+const useStyles = makeStyles((theme) => ({
+  content: {
+    textAlign: "center",
+    margin: "auto",
+  },
+}));
 
-function PageNotFound(props) {
-
-  const history = useHistory();
-
-  const handleLogout = () => {
-    props.dispatch(logoutUser())
-    history.push('/login');
-  };
-  return (
-    <div>
-      <div style={{ textAlign: "center" }}>
-        <Link to="/login">Go to Home </Link>
-        <div onClick={handleLogout}>Logout</div>
-      </div>
-    </div>
-  )
-}
+const NotFoundView = () => (
+  <>
+    <Helmet>
+      <title>404</title>
+    </Helmet>
+    <Box>
+      <Container maxWidth="md">
+        <Typography align="center" color="textPrimary" variant="h5">
+          A página que você está procurando não existe
+        </Typography>
+        <Typography align="center" color="textPrimary" variant="subtitle2">
+          Página não encontrada
+        </Typography>
+        <Box sx={{ textAlign: "center" }}>
+          <img
+            alt="Under development"
+            src="https://react-material-dashboard.devias.io/static/images/undraw_page_not_found_su7k.svg"
+            style={{
+              textAlign: "center",
+              maxWidth: "100%",
+            }}
+          />
+        </Box>
+      </Container>
+    </Box>
+  </>
+);
 
 const mapStateToProps = (store) => ({
-  user: store.user
+  user: store.user,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  dispatch
+  dispatch,
 });
 
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps)
-)(PageNotFound);
+export default compose(connect(mapStateToProps, mapDispatchToProps))(
+  NotFoundView
+);
